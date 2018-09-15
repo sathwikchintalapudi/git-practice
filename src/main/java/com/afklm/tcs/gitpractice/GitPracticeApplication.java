7f6b25d32d3171c248bc743eca745ae9a6c5065c
sathwik <krishna.chintalapudi@klm.com>
2018-09-15 17:15:51 +0530

    GP - Inital practice - master

---
+++ src/main/java/com/afklm/tcs/gitpractice/GitPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.afklm.tcs.gitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeApplication.class, args);
+	}
+}
